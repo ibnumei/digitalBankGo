555eaa34f02c7ca5746f768942dd70efef2d99b1
IBNU.MEIRIYANTO <IBNU.MEIRIYANTO@btpn.com>
2023-08-10 13:20:50 +0700

    add  viper and still failed

---
+++ util/config.go
@@ -0,0 +1,28 @@
+package util
+
+import "github.com/spf13/viper"
+
+//Config stores all configuration of the application.
+//The values are read by viper from  a config file  or env variables
+type Config struct {
+	DBDriver      string `mapStructure:"DB_DRIVER"`
+	DBSource      string `mapStructure:"DB_SOURCE"`
+	ServerAddress string `mapStructure:"SERVER_ADDRESS"`
+}
+
+//LoadConfig reads configuration  from  file or env file
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env") //can read  json xml  and another
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
