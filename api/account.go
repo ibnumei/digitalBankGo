bd5c9ef7edc974ebab2d326994edb8421ddc12d9
IBNU.MEIRIYANTO <IBNU.MEIRIYANTO@btpn.com>
2023-08-24 20:55:18 +0700

    finish 2.0.14

--- api/account.go
+++ api/account.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/ibnumei/digitalBankGo/db/sqlc"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -28,6 +30,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if postgresError, ok := err.(*pq.Error); ok  {
+			switch postgresError.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
